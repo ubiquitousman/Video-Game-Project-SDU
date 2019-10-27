c6d4ea7de4b934594bc0d31b8b3aa03770e4a5fd
Frederik Christensen <frederik.d.c96@gmail.com>
2019-10-27 15:45:47 +0100

    Added TextController Script

---
+++ LabAttack/Assets/Scripts/TextController2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController2 : MonoBehaviour
+{
+    public PlayerController thePlayer; // Reference for spilleren
+
+    private Vector3 lastPlayerPosition; // Den løbende position for spilleren, før teksten bliver flyttet
+    private float distanceToMoveX; // Hvor meget skal teksten flyttes af x-aksen
+    private float distanceToMoveY; // Hvor meget skal teksten flyttes af y-aksen
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>(); // Vi finder spiller komponenten for at kunne bruge den i vores kode
+        lastPlayerPosition = thePlayer.transform.position; // Spillerens position tjekkes ved start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToMoveX = thePlayer.transform.position.x - lastPlayerPosition.x; // Distancen på x-aksen sættes til differencen mellem spillerens nuværende position (den har lige flyttet sig grundet PlayerController-scriptet) og den sidste position (før spilleren blev flyttet)
+        distanceToMoveY = thePlayer.transform.position.y - lastPlayerPosition.y; // Distancen på y-aksen sættes til differencen mellem spillerens nuværende position (den har lige flyttet sig grundet PlayerController-scriptet) og den sidste position (før spilleren blev flyttet)
+        transform.position = new Vector3(transform.position.x + distanceToMoveX, transform.position.y + distanceToMoveY, transform.position.z); // Kameraet flyttes den samme mængde som spilleren er blevet
+
+        lastPlayerPosition = thePlayer.transform.position; // Spillerens nye position tjekkes
+
+    }
+}
