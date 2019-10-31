6a182d9a6d82f56cf5487a8cee035068b469f755
Nickolai <denbredskyggedehat@gmail.com>
2019-10-31 10:57:19 +0100

    Mainscene
    sync and pollish

--- LabAttack/Assets/Scripts/Game/platformMoving.cs
+++ LabAttack/Assets/Scripts/Game/platformMoving.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class platformMoving : MonoBehaviour
 {
+    public Transform pos1, pos2;
+    public float speed;
+    public Transform startPos;
 
-    public Transform Position 1 (left), 
+    Vector3 nextPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextPos = startPos.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void fixedUpdate()
     {
-        
+        if(transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        if(transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
     }
 }
