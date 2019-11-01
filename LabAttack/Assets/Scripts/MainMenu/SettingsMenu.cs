6c0a13de0205e597ef43e08bcf941b9c32921c57
Frederik Christensen <frederik.d.c96@gmail.com>
2019-11-02 00:23:58 +0100

    Made a Slider for Sound Settings (but it doesn't work yet)

---
+++ LabAttack/Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("volume", 0.75f);
+    }
+
+
+    public void SetVolume (float volume)
+    {
+        float sliderValue = slider.value;
+
+        audioMixer.SetFloat("volume", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("volume", volume);
+        Debug.Log(volume);
+    }
+
+
+}
