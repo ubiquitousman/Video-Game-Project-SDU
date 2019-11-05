67d80e5576f4f332825386c72ea4061455b8354a
Alexander <alexandersvanholm@outlook.com>
2019-11-05 02:56:41 +0100

    Animated background and updated flask-HUD (life)

---
+++ LabAttack/Assets/backgroundAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class backgroundAnimation : MonoBehaviour
+{
+    public Sprite[] animatedImages;
+    public SpriteRenderer animateImagebg;
+    // Update is called once per frame
+    void Update()
+    {
+        animateImagebg.sprite = animatedImages[(int)(Time.time * 10) % animatedImages.Length];
+    }
+}
