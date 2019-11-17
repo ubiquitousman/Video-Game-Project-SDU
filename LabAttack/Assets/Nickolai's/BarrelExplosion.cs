e5f1f2f2bffbb56a6bbe4da85cb688d6e4d1331c
Nickolai <nipet18@student.sdu.dk>
2019-11-17 20:36:11 +0100

    Collectables
    monster, collectables, health,

---
+++ LabAttack/Assets/Nickolai's/BarrelExplosion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelExplosion : MonoBehaviour
+{
+     
+
+    
+
+
+    private void OnCollisionEnter(Collision2D other)
+    {
+
+        if (other.gameObject.tag == "Platform")
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+
+    
+}
---
+++ LabAttack/Assets/Nickolai's/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //trasformere det gælde object til at rotere via de 3 axer og gange det med time.deltaTime for at den skal gøre det i bløde overgange pr frame
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime * speed);
+        
+    }
+}
