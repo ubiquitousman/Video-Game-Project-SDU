9c13642dffbb35c0381a4b1f6eb7dacc91eea335
Frederik Christensen <frederik.d.c96@gmail.com>
2019-11-21 19:21:57 +0100

    Fixed small bug with missing scripts and sprites after implementation

--- LabAttack/Assets/Nickolai's/BarrelExplosion.cs
+++ LabAttack/Assets/Nickolai's/BarrelExplosion.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BarrelExplosion : MonoBehaviour
-{
+{/*
      
 
     
@@ -22,4 +22,4 @@ public class BarrelExplosion : MonoBehaviour
     }
 
     
-}
+*/}
